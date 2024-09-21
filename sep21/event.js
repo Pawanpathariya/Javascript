93aa1a90ba8cc20e6e1f30595f7fb8da1754749a
Pawanpathariya <pawanpathariys@gmail.com>
2024-09-21 11:21:46 +0530

    task

--- sep21/event.js
+++ sep21/event.js
@@ -4,3 +4,20 @@ document.getElementById("btn1").addEventListener("click",display);
 function display(){
     alert("Welcome to Cybrom");add
 }*/
+
+
+document.getElementById("btn1").addEventListener("click", cal);
+
+function cal(){
+    let phy=Number(document.getElementById("phy").value);
+    let che=Number(document.getElementById("che").value);
+    let math=Number(document.getElementById("math").value);
+    let eng=Number(document.getElementById("eng").value);
+    let hin=Number(document.getElementById("hin").value);
+   
+    let total=phy+che+math+eng+hin;
+    let per=total/5;
+
+    document.getElementById("ans1").innerHTML="Total marks "+total;
+    document.getElementById("ans2").innerHTML="Percantage "+per+"%";
+}
